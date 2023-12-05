66f07502d9c898f4e7211c813c28092162889020
Mariana Branco <73316700+mbbranco@users.noreply.github.com>
2023-12-05 20:55:07 +0000

    Update day_5.py

--- day_5.py
+++ day_5.py
@@ -5,61 +5,52 @@ txt = []
 for line in sys.stdin:
   line = line.rstrip()
   txt.append(line)
+  
   # print(f'Message from stdin: {line}')
   # break
-print(txt)
+# print(txt)
 
 seeds = txt[0].split(": ")[1]
 seeds = list(seeds.split(" "))
 seeds = [int(s) for s in seeds]
 
 map_name_dict = {}
-map_nr = []
-map_name = ""
-counter = 0
-for s in txt[1:]:
-  print(s)
-  if "map:" in s:
-    map_name_dict[s] = map_nr
-    map_nr = []
+map_code = []
 
-  if s!="" and ":" not in s:
-    nrs = s.split(" ")
-    nrs = [int(nr) for nr in nrs]
-    map_nr.append(nrs)
+txt = txt[2:]
+txt.append("")
 
-print(seeds)
+for t in txt:
+  # print(t)
+  if ":" in t:
+    map_name = t
+  elif t=="":
+    map_name_dict[map_name] = map_code
+    map_code = []
+  else:
+    list_t = [int(t) for t in t.split(" ")]
+    map_code.append(list_t)
+
 print(map_name_dict)
 
-destinations = []
-sources = []
-map_name_dict_final = {}
-dict_v = {}
-for map_name,nrs in map_name_dict.items():
-    print(map_name)
-    map_name_dict_final[map_name] = []
-    for l in nrs:
-      print(l)
-      ini = l[0]
-      fin = l[1]
-      leng = l[2]
-      sour = list(range(fin,fin+leng))
-      dest = list(range(ini,ini+leng))
 
-      for i,d in enumerate(dest):
-        dict_v[sour[i]] = d
-
-    map_name_dict_final[map_name].append(dict_v)
-    dict_v = {}
-    
+final_locations = {}
 for s in seeds:
-  print(f'Seed: {s}')
-  current_val = int(s)
-  for k,val in map_name_dict_final.items():
-    print(k)
-    print(val)
-    dict_use = val[0]
-    if current_val  in dict_use.keys():
-      current_val = dict_use[current_val]
-    print(current_val)
-        
+  seed = int(s)
+  print(f'\nSeed: {seed}')
+  current_val = seed
+  found = False
+
+  for k,val in map_name_dict.items():
+    for v in val:
+      if current_val < v[1] or current_val > v[1] + v[2]:
+        next
+      else:
+        found = True
+        current_val = current_val + v[0] - v[1]
+        break
+
+  final_locations[s] = current_val
+
+print(final_locations)
+print(min(final_locations.values()))
