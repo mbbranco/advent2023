8da0380f76b81e4547df5c316b98fa351957fa4f
Mariana Branco <73316700+mbbranco@users.noreply.github.com>
2023-12-13 23:04:15 +0000

    Update day_11.py

--- day_11.py
+++ day_11.py
@@ -13,16 +13,16 @@ for t in txt:
 
 print(f'N Rows: {len(maps)} | N Cols: {len(maps[0])}')
 
-empty_rows = []
 # check rows
+empty_rows = []
 for i,row in enumerate(maps):
   if row.count("#") == 0:
     empty_rows.append(i)
 
 print(empty_rows)
   
-empty_cols = []
 # check columns
+empty_cols = []
 n_columns = len(maps[0])
 for j in range(0,n_columns):
   not_found = True
@@ -34,23 +34,6 @@ for j in range(0,n_columns):
     empty_cols.append(j)
 
 print(empty_cols)
-
-# expand rows
-new_pos = 0
-for pos in empty_rows:
-  final_pos = pos + new_pos
-  list_add = ["."]*n_columns
-  maps.insert(final_pos,list_add)
-  new_pos+=1
-
-
-# expand cols
-new_pos = 0
-for pos in empty_cols:
-  final_pos = pos + new_pos
-  for i, row in enumerate(maps):
-    maps[i].insert(final_pos,".")
-  new_pos+=1
 
 
 print(f'N Rows: {len(maps)} | N Cols: {len(maps[0])}')
@@ -73,20 +56,34 @@ combi = []
 for i in range(1,counter):
   for j in range(i+1,counter):
     combi.append([i,j])
+
+size_expand = 1000000-1
+new_coords = {}
+for k,val in counter_dict.items():
+  counter_cols = 0
+  for i in empty_cols:
+    if val[1]>=i:
+      counter_cols+=size_expand
     
+  counter_rows = 0
+  for j in empty_rows:
+    if val[0]>=j:
+      counter_rows+=size_expand
+      
+  new_coords[k] = [val[0]+counter_rows,val[1]+counter_cols]
+
 combi_dict = {}
 n_steps_needed = 0
 for c in combi:
   gal_a = c[0]
   gal_b = c[1]
   combi = str(gal_a)+" => "+str(gal_b)
-  coords_a = counter_dict[gal_a]
-  coords_b = counter_dict[gal_b]
+  coords_a = new_coords[gal_a]
+  coords_b = new_coords[gal_b]
   
   dif_x = abs(coords_a[0] - coords_b[0])
   dif_y = abs(coords_a[1] - coords_b[1])
   n_steps_needed = dif_x + dif_y
   combi_dict[combi] = n_steps_needed
-
 # print(combi_dict)
 print(sum(combi_dict.values()))
