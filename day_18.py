7c55979bf1407adac8260acebb9bd894d8e43522
Mariana Branco <73316700+mbbranco@users.noreply.github.com>
2024-01-09 22:26:56 +0000

    Update day_18.py

--- day_18.py
+++ day_18.py
@@ -9,51 +9,63 @@ for line in sys.stdin:
   # break
 print(txt)
 
+
 pos = [0,0]
-result = [(0,0)]
-dict_row = {}
+rows = []
 for t in txt:
-  direction = t[0]
+  # print(t)
+  start = pos
+
+  direction = t.split(" ")[0]
   nr = int(t.split(" ")[1])
-  print(direction,nr)
-  print(pos)
-  if direction == 'R':
-    new_pos = [pos[0],pos[1]+nr]
-    range_x = [pos[0]]*(nr+1)
-    range_y = list(range(pos[1],pos[1]+nr+1))
+  
+  y_move = False
+  x_move = False
+  if direction == 'U':
+    for r in range(start[1],start[1] - nr - 1, -1):
+      rows.append([start[0],r])
+      
   elif direction == 'D':
-    new_pos = [pos[0]+nr,pos[1]]
-    range_y = [pos[1]]*(nr+1)
-    range_x = list(range(pos[0],pos[0]+nr+1))
-  elif direction == 'U':
-    new_pos = [pos[0]-nr,pos[1]]
-    range_y = [pos[1]]*(nr+1)
-    range_x = list(range(pos[0]-nr,pos[0]+1))
+    for r in range(start[1], start[1] + nr + 1):
+      rows.append([start[0],r])
+      
+  elif direction == 'R':
+    for r in range(start[0], start[0] + nr + 1):
+      rows.append([r,start[1]])
+      
   elif direction == 'L':
-    new_pos = [pos[0],pos[1]-nr]
-    range_x = [pos[0]]*(nr+1)
-    range_y = list(range(pos[1]-nr,pos[1]+1))
+    for r in range(start[0],start[0] - nr - 1,-1):
+      rows.append([r,start[1]])
 
+  pos = rows[-1]
+  
+max_row_dict = {}
+min_row_dict = {}
+max_col_dict = {}
+min_col_dict = {}
 
-  left = range_y[0]
-  right = range_y[-1]
-  for x in range_x:
-    if x in dict_row.keys():
-      val = dict_row[x]
-      dict_row[x] = (min(val[0],left),max(val[1],right))
-    else:
-      dict_row[x] = (left,right)
+for r in rows:
+  x = r[0]
+  y = r[1]
+  
+  if y in max_row_dict.keys():
+    if x > max_row_dict[y]:
+      max_row_dict[y] = x
+  else:
+    max_row_dict[y] = x
+    
+  if y in min_row_dict.keys():
+    if x < min_row_dict[y]:
+      min_row_dict[y] = x
+  else:
+    min_row_dict[y] = x
 
-  # result.extend(range_v)
-  pos = new_pos
-print(dict_row)
-# print(result)
-# print(sorted(result))
+# print(max_row_dict)
+# print(min_row_dict)
 
-nr_houses = 0
-for k, val in dict_row.items():
-  dif = val[1]-val[0]+1
-  print(dif)
-  nr_houses+=abs(dif)
-
-print(nr_houses)
+total = 0
+for k, val in max_row_dict.items():
+  nr_ones = max_row_dict[k]-min_row_dict[k]+1
+  # print(nr_ones)
+  total +=nr_ones
+print(total)
